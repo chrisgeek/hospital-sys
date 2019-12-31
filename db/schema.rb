0d43e452600892581f4d71ec416ba088ca42a59c
Opara Ifeanyi Christian <chrisgeek29@gmail.com>
2019-12-31 01:14:48 +0000

    added travis.yml

--- db/schema.rb
+++ db/schema.rb
@@ -44,14 +44,14 @@ ActiveRecord::Schema.define(version: 2019_09_05_135409) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "role", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
+    t.string "role", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
