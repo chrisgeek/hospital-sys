7724412dd4be549a89f214864e66f639dd81cb7c
Opara Ifeanyi Christian <chrisgeek29@gmail.com>
2020-01-05 16:09:25 +0000

    fixed failing spec

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Appointment, type: :model do
   describe 'Associations' do
     it { is_expected.to belong_to(:user).optional }
-    it { is_expected.not_to belong_to :doctor }
+    it { is_expected.to belong_to :doctor }
     it { is_expected.to belong_to :patient }
   end
 
